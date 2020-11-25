e53c8099a46be35d3b7e18391437daa897d83354
El-akama <akmatjanovel@gmail.com>
2020-11-25 18:09:10 +0600

    initial commit

---
+++ task_oop_incapsulation.py
@@ -0,0 +1,33 @@
+# task1
+# class Car:
+
+#     def __init__(self, make, model, year, odometer=0, fuel=70):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer = odometer
+#         self.fuel = fuel
+
+#     def __add_distance(self, km):
+#         self.odometer += km
+
+#     def __subtract_fuel(self, fuel):
+#         self.fuel -= fuel
+
+#     def drive(self, km):
+#         if self.fuel * 10 >= km:
+#             self.__add_distance(km)
+#             kml = km // 10
+#             self.__subtract_fuel(kml)
+#             print('Let’s drive!')
+#         else:
+#             print('Need more fuel!')
+
+# a = Car('japan', 'toyota', 2020, 70)
+# a.drive(100)
+# print(a.odometer)
+# print(a.fuel)
+
+# task2 
+# task3
+# делаю
