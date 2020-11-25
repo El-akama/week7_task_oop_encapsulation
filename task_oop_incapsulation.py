9a61b27e8c92df851549e668a6a64cc0560d54e6
El-akama <akmatjanovel@gmail.com>
2020-11-25 18:39:25 +0600

    modified

--- task_oop_incapsulation.py
+++ task_oop_incapsulation.py
@@ -29,5 +29,54 @@
 # print(a.fuel)
 
 # task2 
+# class Mobile:
+
+#     __imeil = "samsung"
+#     __battery = 100
+#     __info = 'ssd'
+#     __os = 'charity'
+
+#     def listen_music(self):
+#         self.__battery -= 5
+#         print(f'играет музыка, заряд батареи {self.__battery} %')
+
+#     def watch_video(self):
+#         self.__battery -=7
+#         print(f"смотрим видео, заряд батареи {self.__battery} %")
+#         if self.__battery <= 10:
+#             print(f"не смотрим видео, заряд батареии {self.__battery} %")
+        
+#     def battery_charging(self):
+#         self.__battery = 100
+#         print('батарея заряжена')
+
+#     def info_battery(self):
+#         if self.__battery == 0:
+#             raise Exception ("батарея разряжена, нужно подзарядить")
+#         else:
+#             print(self.__battery)
+
+# m = Mobile()
+# m.listen_music()
+# m.listen_music()
+# m.listen_music()
+# m.listen_music()
+# m.listen_music()
+# m.listen_music()
+# m.watch_video()
+# m.watch_video()
+# m.watch_video()
+# m.watch_video()
+# m.watch_video()
+# m.watch_video()
+# m.watch_video()
+# m.watch_video()
+# # m.watch_video()
+# # m.watch_video()       # чтобы вызвать ошибку
+# m.info_battery()
+# m.battery_charging()
+# m.info_battery()
+
+
 # task3
 # делаю
